f932a0b3b15effc626486cd1590d877250383891
chamelek <40717161+chamelek@users.noreply.github.com>
2019-03-29 13:51:26 +0200

    Add files via upload

--- main.rb
+++ main.rb
@@ -1,67 +1,73 @@
+module GetData
+  # Get from DB or file
+  def get_students
+    students = []
+    students << Student.new("Volodymyr Stetsyshyn", 3)
+    students << Student.new("Vasya Naguliak", 70)
+    students << Student.new("Nazar Chaban", 50)
+    students << Student.new("Luibomyr Xanas", 100)
+    students
+  end
+end
+
 class University
-  attr_accessor :name, :students
+  attr_accessor :name
+  attr_reader :students
 
   def initialize(name)
     @name = name
     @students = []
   end
 
-  def addStudent(student)
+  def add_student(student)
     @students << student
     student
-  end
-
-  def selection 
-  	if @students.size != 0
-  	 points = @students.map(&:point)
-  	 average = points.inject { |sum, n| sum + n } / @students.size.to_f
-  	 @students.select { |student| student.point >= average }
-  	end  
   end
 end
 
 class Student
+  extend GetData
   attr_accessor :name, :point
 
   def initialize(name, point)
     @name = name
-    @point = point
+    @point = point.to_f
   end
-
 end
-
 
 class InternShip
-  attr_accessor :name, :students
+  attr_accessor :name
+  attr_reader :students
 
-  def initialize(name)
+  def initialize(name,university)
     @name = name
-    @students = []
+    @university = university
+    @students = get_smart_students
   end
-
-  def get_smart_students(fromUniversity)
-  	@students = fromUniversity
-	end
 
   def print
-  	if @students!= nil
     @students.each do |student|
       pp "Name: #{student.name}. Point: #{student.point}"
-    	end
-    else 
-  	 p "Zero smart students"
-  	end
+    end
+  end
+
+  private
+
+  def get_smart_students
+
+  return [] if @university.students.size.zero? 
+
+    average = @university.students.map(&:point).inject(0, :+) / @university.students.size.to_f
+    @university.students.select { |student| student.point > average }
   end
 end
-
 
 class Application
   university = University.new("LNU")
-  university.addStudent(Student.new("Nazar Chaban", 5))
-  university.addStudent(Student.new("Luibomyr Xanas", 2))
-  
-  internship = InternShip.new("InterLink")
- 
-  internship.get_smart_students(university.selection)
+  students = Student.get_students  
+
+  students.each { |el| university.add_student(el) }
+
+  internship = InternShip.new("InterLink",university)
   internship.print
 end
